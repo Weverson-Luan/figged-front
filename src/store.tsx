d1a556febb8d747c8269b69009e2318497310502
Weverson-Luan <wltech2022@gmail.com>
2023-06-06 15:52:02 -0300

    code-style :art: removendo string

--- src/store.tsx
+++ src/store.tsx
@@ -6,7 +6,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-  ddaf
 declare global {
     interface Window {
         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
